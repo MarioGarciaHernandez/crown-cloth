105333bc2df70231083c03f51cfe37a1ac0cb481
Mario Garcia Hernandez <mario-garcia-h@hotmail.com>
2020-11-07 20:52:00 -0500

    minor change to titles

--- src/homepage/homepage.component.jsx
+++ src/homepage/homepage.component.jsx
@@ -6,19 +6,19 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">Hats</h1>
+          <h1 className="title">Hat</h1>
           <span className="subtitle">Shop Now</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">Jackets</h1>
+          <h1 className="title">Jacket</h1>
           <span className="subtitle">Shop Now</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">Sneakers</h1>
+          <h1 className="title">Sneaker</h1>
           <span className="subtitle">Shop Now</span>
         </div>
       </div>
